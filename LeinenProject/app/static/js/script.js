6344fb93da36ad0e04b757470307f3d60f32da06
Paluja <jtp1006@hotmail.com>
2023-02-24 16:19:06 +0100

    Queda arregar el puto ver mas

--- LeinenProject/app/static/js/script.js
+++ LeinenProject/app/static/js/script.js
@@ -42,6 +42,7 @@ $('.owl-carousel').owlCarousel({
     margin:5,
     responsive:true,
     fluidSpeed:true,
+    
 
     navText:[
         prevIcon,
@@ -61,24 +62,3 @@ $('.owl-carousel').owlCarousel({
 })
 
 // video carousel
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
-        }
-    }
-})
